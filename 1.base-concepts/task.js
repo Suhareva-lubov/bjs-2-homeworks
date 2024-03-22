30b557cded2660b1095325e5ac347d196bf81403
Sukhareva <L.Sukhareva@X5.ru>
2024-03-22 14:56:15 +0300

    Задание по занятию "Основные понятия"

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant >= 0) {
+    if(discriminant > 0) {
+      arr.push((-b + Math.sqrt(discriminant)) / (2*a));
+      arr.push((-b - Math.sqrt(discriminant)) / (2*a));
+
+    } else {arr.push(-b/(2*a))}
+  } 
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let s = amount - contribution;
+  let p = percent / 100 / 12;
+  let n = countMonths;
+  let payMounth = s * (p + (p / ((Math.pow(1+p, n)) - 1)));
+  let count = parseFloat((payMounth * countMonths).toFixed(2));
+
+  return count;
   
 }
