e9c1f96b37c431ee3aea5d5b0a446ff7c0d2d1f0
Sukhareva <L.Sukhareva@X5.ru>
2024-04-10 23:29:58 +0300

    Homeworks Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
   
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
+    if(this.hasOwnProperty("marks")) {
+        let arr = this.marks;
+        this.marks = [...arr, ...marks];
+    }
   
 }
 
 Student.prototype.getAverage = function () {
+    if (this.hasOwnProperty("marks") && this.marks.length != 0) {
+        let sum = this.marks.reduce((acc, item) => {return acc + item}, 0);
+        return parseFloat((sum / this.marks.length).toFixed(2));
+    } else return 0;
   
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;  
 }
