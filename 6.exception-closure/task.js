9612a89fadfb154d5eb9a53af1fe2216f6ace419
Sukhareva <L.Sukhareva@X5.ru>
2024-05-22 18:36:42 +0300

    Задание по занятию Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,47 @@
-﻿
+﻿function parseCount(number) {
+        if (Number.isNaN(Number.parseFloat(number))) {
+            throw new Error("Невалидное значение");
+          };
+    
+        return Number.parseFloat(number);
+    }; 
+
+function validateCount(string) {
+    try {
+        return parseCount(string);
+    } catch(error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if(a + b > c && a + c > b && b + c > a ) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        } else {throw new Error("Треугольник с такими сторонами не существует") } 
+    }
+
+    get perimeter() {
+        return parseFloat(this.a + this.b + this.c) ;
+    }
+
+    get area() {
+        let p = (this.perimeter / 2);
+        return parseFloat(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+        
+    } catch(err) {
+        return {
+            get area() {return "Ошибка! Треугольник не существует";},
+            get perimeter() {return "Ошибка! Треугольник не существует";}
+        };
+    }
+
+}
